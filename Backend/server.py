60c26843c5ac6f5f722a42082e4208967fa10879
lightcoxa <andreyap@ymail.com>
2022-07-15 11:34:10 +0800

    update

--- Backend/server.py
+++ Backend/server.py
@@ -1,10 +1,9 @@
-from django.shortcuts import redirect, render
-from flask import Flask, render_template, request, session, flash, url_for
+from flask import Flask, render_template, request, session, flash, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
-from flask_login import LoginManager, login_required, UserMixin, login_user
+from flask_login import LoginManager, login_required, UserMixin, login_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import time, os, hashlib
+import time, os, requests, urllib.parse, json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -12,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
-login_manager = LoginManager
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
@@ -51,26 +50,29 @@ class Mappings(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     cylinderNum = db.Column(db.Integer, unique=True, nullable=False)
-    medicationName = db.Column(db.String(100), nullable=False)
+    medicationName = db.Column(db.String(255), nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
 
 class Users(UserMixin, db.Model):
     __tablename__= 'users'
-    def __init__(self, username, password):
+    def __init__(self, username, password, number):
         self.username = username
         self.password = password
+        self.number = number
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.string(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    number = db.Column(db.String(25), nullable=False)
 
 # create db and tables if don't exist
 db.create_all()
 print(Mappings.query.all())
 
 # User creation for testing purposes
-user = Users(username="test", password=generate_password_hash("password"))
-db.session.add(user)
-db.session.commit()
+# user = Users(username="test", password=generate_password_hash("password"), number="90202365")
+# db.session.add(user)
+# db.session.commit()
 
 # temp = Mappings(1,"ibuprofen")
 # temp2 = Mappings(2, "paracetamol")
@@ -97,6 +99,35 @@ def login():
             return redirect(url_for("login"))
     else:
         return "login page"
+
+@app.route("/setup", methods=["GET", "POST"])
+def setup():
+    if request.method == "POST":
+        requestData = request.get_json()
+        uname = requestData['username']
+        pword = requestData['password']
+        phoneNumber = requestData['phoneNumber']
+        mappingData = requestData['mapping']
+        stockData = requestData['stocks']
+
+        user = Users(username=uname, password=generate_password_hash(pword), number=phoneNumber)
+        db.session.add(user)
+        newMapping1 = Mappings.query.filter_by(cylinderNum=1).update(dict(stock=stockData['cyl1'], medicationName=mappingData['cyl1']))
+        newMapping2 = Mappings.query.filter_by(cylinderNum=2).update(dict(stock=stockData['cyl2'], medicationName=mappingData['cyl2']))
+        newMapping3 = Mappings.query.filter_by(cylinderNum=3).update(dict(stock=stockData['cyl3'], medicationName=mappingData['cyl3']))
+        newMapping4 = Mappings.query.filter_by(cylinderNum=4).update(dict(stock=stockData['cyl4'], medicationName=mappingData['cyl4']))
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    else:
+        users = []
+        for user in Users.query.all():
+            users.append(user.__dict__)
+        if users:
+            return redirect(url_for('login'))
+        return render_template("")
+
 
 @app.route("/", methods=["GET"])
 @login_required
@@ -130,22 +161,73 @@ def config():
 @login_required
 def settings():
     if request.method == "POST":
-        return
+        requestData = request.get_json()
+        phoneNumber = requestData['phoneNumber']
+        try:
+            user = Users.query.filter_by(username=current_user.username).update(dict(number=int(phoneNumber)))
+        except:
+            flash('Error changing number, please check number and try again.')
+            return redirect(url_for('settings'))
     else:
-        return
+
+        return current_user.number
 
 @app.route("/medsettings", methods=["GET", "POST"])
 @login_required
 def medSettings():
     if request.method == "POST":
-        return
-    else:
-        return
+        requestData = request.get_json()
+        dosageData = requestData['dosage']
+        stockData = requestData['stock']
+        mappingData= requestData['mapping']
+        update1 = Mappings.query.filter_by(cylinderNum=1).update(dict(stock=stockData['cyl1'], medicationName=mappingData['cyl1']))
+        update2 = Mappings.query.filter_by(cylinderNum=2).update(dict(stock=stockData['cyl2'], medicationName=mappingData['cyl2']))
+        update3 = Mappings.query.filter_by(cylinderNum=3).update(dict(stock=stockData['cyl3'], medicationName=mappingData['cyl3']))
+        update4 = Mappings.query.filter_by(cylinderNum=4).update(dict(stock=stockData['cyl4'], medicationName=mappingData['cyl4']))
 
-# Endpoint for raspberry to retr config
+        flash("Successfully changed settings")
+        return redirect(url_for('medSettings'))
+        
+    else:
+        dosage = Medications.query.all()
+        
+        return render_template(".html",
+        stock1=Mappings.query.filter_by(cylinderNum=1).first().stock,
+        stock2=Mappings.query.filter_by(cylinderNum=2).first().stock,
+        stock3=Mappings.query.filter_by(cylinderNum=3).first().stock,
+        stock4=Mappings.query.filter_by(cylinderNum=4).first().stock,
+        mapping1=Mappings.query.filter_by(cylinderNum=1).first().medicationName,
+        mapping2=Mappings.query.filter_by(cylinderNum=2).first().medicationName,
+        mapping3=Mappings.query.filter_by(cylinderNum=3).first().medicationName,
+        mapping4=Mappings.query.filter_by(cylinderNum=4).first().medicationName)
+
+# Endpoint for raspberry
 @app.route("/retrconfig", methods=["GET"])
 @auth.login_required
 def retrconfig():
+    data = Medications.query.all()
+
+@app.route("/sendmessage", methods=["POST"])
+@auth.login_required
+def sendMessage():
+    number = Users.query.first().number
+    message = ""
+    x = requests.get(f"https://api.callmebot.com/whatsapp.php?phone={number}&text={urllib.parse.quote_plus(message)}&apikey=[your_apikey]")
+
+@app.route("/getstock", methods=["GET"])
+@auth.login_required
+def getStock():
+    stock = {
+        "cyl1": Mappings.query.filter_by(cylinderNum=1).first().stock,
+        "cyl2": Mappings.query.filter_by(cylinderNum=2).first().stock,
+        "cyl3": Mappings.query.filter_by(cylinderNum=3).first().stock,
+        "cyl4": Mappings.query.filter_by(cylinderNum=4).first().stock
+    }
+    return stock
+
+@app.route("/updateStock", methods=["GET"])
+@auth.login_required
+def updateStock():
     pass
 
 if __name__ == "__main__":
